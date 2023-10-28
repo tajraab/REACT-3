e63489358eaedc184fbe2d358fd60b8a2fdae493
tajraab <tajrabacevac@gmail.com>
2023-10-28 15:57:10 +0200

    domaci

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./HomePage.css";
+
+ function HomePage() {
+  return (
+    <div className="container">
+        Ovo je pocetna stranica.U ovom hederu ima jos stavki kao sto su produkti i postovi.
+
+    </div>
+  )
+}
+export default HomePage;
