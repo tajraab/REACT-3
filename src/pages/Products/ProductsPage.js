45c01d17200ec88c0f4003db539559531f62efcc
tajraab <tajrabacevac@gmail.com>
2023-10-30 17:25:12 +0100

    products

--- src/pages/Products/ProductsPage.js
+++ src/pages/Products/ProductsPage.js
@@ -1,6 +1,10 @@
 import React from "react";
-
+// import React,{useState,}
+// import {useNavigate}from "react-router-dom";
+// import axios "axios";
 function ProductsPage() {
+//   const[data,setData]=useState([]);
+//   const navigate=useNavigate()
   return (
     <div style={{ backgroundColor: "gray", minHeight: "90vh" }}></div>
   );
